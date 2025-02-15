d397589824cb8e57582262734d749bae55b4b702
mahdihasan7 <mahdihasan71999@gmail.com>
2025-02-15 23:46:45 +0600

    Pushing to github

---
+++ calendarMaker.py
@@ -0,0 +1,10 @@
+import datetime
+
+# Set up the constants:
+DAYS = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
+MONTHS = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
+
+print('Calendar Maker')
+
+while True: # Loop to get a year from the user.
+    print('')
